374a0bde2f0014ef00a572b57f405693ab15277d
Sch1mith <165226331+Sch1mith@users.noreply.github.com>
2025-04-14 17:05:17 -0300

    Add files via upload

---
+++ skillcode/src/main/java/com/grupo3/skillcode/SkillcodeApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.skillcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillcodeApplication.class, args);
+	}
+
+}
---
+++ skillcode/src/test/java/com/grupo3/skillcode/SkillcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.skillcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
